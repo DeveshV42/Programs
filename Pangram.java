8782f680f06cad92850c442d0897dfe05f0336ae
DeveshV42 <133776613+DeveshV42@users.noreply.github.com>
2023-11-06 12:08:26 +0530

    Add files via upload

---
+++ Pangram.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+
+public class PangramChecker {
+    public static void main(String[] args) {
+        String input = "The quick brown fox jumps over the lazy dog";
+        boolean isPangram = isPangram(input);
+
+        if (isPangram) {
+            System.out.println("The input is a pangram.");
+        } else {
+            System.out.println("The input is not a pangram.");
+        }
+    }
+
+    public static boolean isPangram(String input) {
+        input = input.toLowerCase();
+
+        HashSet<Character> characterSet = new HashSet<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+
+            if (c >= 'a' && c <= 'z') {
+                characterSet.add(c);
+            }
+        }
+
+        return characterSet.size() == 26;
+    }
+}
---
+++ Roman.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class RomanToInteger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Roman numeral: ");
+        String romanNumeral = scanner.nextLine();
+        scanner.close();
+
+        int result = romanToInt(romanNumeral);
+        System.out.println("Integer representation: " + result);
+    }
+
+    public static int romanToInt(String s) {
+        Map<Character, Integer> romanValues = new HashMap<>();
+        romanValues.put('I', 1);
+        romanValues.put('V', 5);
+        romanValues.put('X', 10);
+        romanValues.put('L', 50);
+        romanValues.put('C', 100);
+        romanValues.put('D', 500);
+        romanValues.put('M', 1000);
+
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currentValue = romanValues.get(s.charAt(i));
+            if (currentValue < prevValue) {
+                result -= currentValue;
+            } else {
+                result += currentValue;
+            }
+            prevValue = currentValue;
+        }
+
+        return result;
+    }
+}
